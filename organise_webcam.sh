2b59e7148771b856a418ab7acedadfc0420c366d
Pierre Cazenave <pwcazenave@gmail.com>
2013-05-29 14:20:09 +0100

    Change the way the tidying loop works by moving all files for a given day in one go instead of moving each file individually (which is very slow when you have lots of files)

--- organise_webcam.sh
+++ organise_webcam.sh
@@ -3,7 +3,7 @@
 # Run this with crontab at midnight to organise the previous day's webcam
 # snapshots into dated subfolders.
 
-BASE=/media/Archive/camera/
+BASE=/media/Archive/
 
 TODAY=${TODAY:-$(date "+%Y-%m-%d")}
 ALTTODAY=${ALTTODAY:-$(date "+%Y%m%d")}
@@ -12,13 +12,13 @@ if [ ! -d $BASE/$TODAY ]; then
     mkdir $BASE/$TODAY
 fi
 
-mv $BASE/78A5DD003664\(003abjn\)_?_${ALTTODAY}*.jpg $BASE/$TODAY
+mv $BASE/78A5DD003664\(003abjn\)_?_${ALTTODAY}*.jpg $BASE/$TODAY || true
 
 # Tidy up any stragglers from yesterday (or any other day).
-for i in $(ls -1 $BASE | grep -v -- - | grep -v $ALTTODAY); do 
-    TODIR=$(echo $i | awk 'OFS="-" {print substr($0,25,4), substr($0,29,2), substr($0,31,2)}')
-    if [ ! -d $TODIR ]; then
-        mkdir $TODIR
-        mv "$i" $TODIR
+for i in $(ls -1 $BASE | grep -v -- - | grep -v $ALTTODAY | awk 'OFS="-" {print substr($0,25,4), substr($0,29,2), substr($0,31,2)}' | sort -u); do 
+    TODIR=$i
+    if [ ! -d $BASE/$TODIR ]; then
+        mkdir $BASE/$TODIR
+        mv $BASE/78A5DD003664\(003abjn\)_?_${TODIR//-/}*.jpg $BASE/$TODIR
     fi
 done
